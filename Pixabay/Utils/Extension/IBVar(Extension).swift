b1693eb11ba512165607143580021daa479813b6
yuqiang <1054572107@qq.com>
2017-11-01 20:17:19 +0800

    add launch

---
+++ Pixabay/Utils/Extension/IBVar(Extension).swift
@@ -0,0 +1,52 @@
+//
+//  IB.swift
+//  Pixabay
+//
+//  Created by sungrow on 2017/11/1.
+//  Copyright © 2017年 yuqiangcoder. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    @IBInspectable var localizedKey: String? {
+        set {
+            guard let newValue = newValue else { return }
+            text = NSLocalizedString(newValue, comment: "")
+        }
+        get { return text }
+    }
+}
+
+extension UIButton {
+    @IBInspectable var localizedKey: String? {
+        set {
+            guard let newValue = newValue else { return }
+            setTitle(NSLocalizedString(newValue, comment: ""), for: .normal)
+        }
+        get { return titleLabel?.text }
+    }
+}
+
+extension UITextField {
+    @IBInspectable var localizedKey: String? {
+        set {
+            guard let newValue = newValue else { return }
+            placeholder = NSLocalizedString(newValue, comment: "")
+        }
+        get { return placeholder }
+    }
+}
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
